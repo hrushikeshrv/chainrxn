ee8b56e3138041e4df655b097db7a463fe5ebf60
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2023-03-21 12:51:07 +0530

    Added svgs for particles

--- static/js/core/chainreaction.js
+++ static/js/core/chainreaction.js
@@ -66,8 +66,38 @@ class Game {
                 `chainrxn-particle-player-${particle.player.id}`
             );
             particleElement.dataset.atomicity = particle.atomicity;
-            particleElement.style.backgroundColor = particle.player.color;
-            particleElement.innerHTML = particle.atomicity;
+            if (particle.atomicity === 1) {
+                particleElement.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle" width="33" height="33" viewBox="0 0 24 24" stroke-width="1.5" stroke="${particle.player.color}" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                  <circle cx="12" cy="12" r="9" />
+                </svg>
+                `;
+            }
+            else if (particle.atomicity === 2) {
+                particleElement.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-atom-2" width="33" height="33" viewBox="0 0 24 24" stroke-width="1.5" stroke="${particle.player.color}" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                  <circle cx="12" cy="12" r="3" />
+                  <line x1="12" y1="21" x2="12" y2="21.01" />
+                  <line x1="3" y1="9" x2="3" y2="9.01" />
+                  <line x1="21" y1="9" x2="21" y2="9.01" />
+                  <path d="M8 20.1a9 9 0 0 1 -5 -7.1" />
+                  <path d="M16 20.1a9 9 0 0 0 5 -7.1" />
+                  <path d="M6.2 5a9 9 0 0 1 11.4 0" />
+                </svg>
+                `
+            }
+            else if (particle.atomicity === 3) {
+                particleElement.innerHTML = `
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-atom" width="33" height="33" viewBox="0 0 24 24" stroke-width="1.5" stroke="${particle.player.color}" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                      <line x1="12" y1="12" x2="12" y2="12.01" />
+                      <path d="M12 2a4 10 0 0 0 -4 10a4 10 0 0 0 4 10a4 10 0 0 0 4 -10a4 10 0 0 0 -4 -10" transform="rotate(45 12 12)" />
+                      <path d="M12 2a4 10 0 0 0 -4 10a4 10 0 0 0 4 10a4 10 0 0 0 4 -10a4 10 0 0 0 -4 -10" transform="rotate(-45 12 12)" />
+                    </svg>
+                `;
+            }
         }
 
         return particleElement;
@@ -102,7 +132,12 @@ class Game {
     }
 
     getMaxAtomicity(row, col) {
-
+        if (row === 0 || row === this.height - 1) {
+            if (col === 0 || col === this.width - 1) return 1;
+            return 2;
+        }
+        if (col === 0 || col === this.width - 1) return 2;
+        return 3;
     }
 
     propagate(row, col) {
